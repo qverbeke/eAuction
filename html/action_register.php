cb1ef8083501a3ac008a656fd7e8b599ba5b3f66
Matt Mancini <matthewrmancini@gmail.com>
2018-04-02 18:49:37 -0400

    checks if username or email are already in our database

--- html/action_register.php
+++ html/action_register.php
@@ -12,44 +12,35 @@ $password = mysqli_real_escape_string($conn, $_POST['password']);
 $email = mysqli_real_escape_string($conn, $_POST['email']);
 $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm-password']);
 
+//If the two passwords dont match, reload the page.
 if($confirm_password != $password){
-  printf("confirm password not the same as password");
+  header("Location: login.php");
+  exit();
 }
-//placeholder for DOB bc we dont have it yet
+//placeholder for values because they are not in the html form as of now
 
 $DOB = "99/99/99";
 $gender = 1;
 $address = "abbey lane";
-/*if(empty($username) || empty($password) ) {
-	header("Location: index.php?signup=empty");
-	exit();
-}*/
-/*
-$sql_statement = "SELECT * FROM Users U WHERE U.username = $username OR U.email = $email";
-$result = mysqli_query($conn, $sql_statement);
-//if there is someone with the username or email
-if(mysqli_num_rows($result) != 0){
-  echo "There is someone with the same username or email already in our database";
-}
-echo "Done";*/
 
+//Check if anyone has the same username or the same email as this new use
 $sql_statement = "SELECT * FROM User_Username UU, User_Email UE  WHERE UU.username = '$username' OR UE.email = '$email';";
 $result = mysqli_query($conn, $sql_statement);
 if(mysqli_num_rows($result) != 0){
-  echo "There is someone with the same username or email already in our database";
+  //reload the page and exit the script
+  header("Location: login.php");
+  exit();
 }
+//Encrypt the password
 $hashed_pwd = password_hash($password, PASSWORD_DEFAULT);
 $sql_statement = "INSERT INTO User(Password, DOB, Address, Gender) VALUES ('$hashed_pwd', '$DOB', '$address', '$gender');";
 mysqli_query($conn, $sql_statement);
-$err = mysqli_error($conn);
-trigger_error($err);
+//Get the uid from the last insert statement
 $UID = mysqli_insert_id($conn);
 $sql_statement = "INSERT INTO User_Username(UID, Username) VALUES ('$UID', '$username');";
 mysqli_query($conn, $sql_statement);
 $sql_statement = "INSERT INTO User_Email(UID, Email) VALUES ('$UID', '$email');";
 mysqli_query($conn, $sql_statement);
-
-/*if(mysqli_num_rows($result) != 0){
-  echo "There is someone with that username";
-}*/
+//go to the main page
+header("Location: index.php?signup=success");
 ?>
