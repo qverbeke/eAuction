fb59ec2da195a27ff40566922c159fa0b5e0b113
matt <matthewrmancini@gmail.com>
2018-04-11 19:19:26 -0400

    gets messages to send to database

--- html/action_send.php
+++ html/action_send.php
@@ -5,14 +5,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once 'connect-to-database.php';
 
-$to = $_POST["to"];
+$Reciever_Username = $_POST["to"];
 $message = $_POST["message"];
 $action = $_POST["action"];
 
 $Sender_UID = $_SESSION["UID"];
 
 if($action == 'send'){
-    $sql_statement = "SELECT U.UID FROM User_Username U WHERE U.Username='$to'";
+    $sql_statement = "SELECT U.UID FROM User_Username U WHERE U.Username='$Reciever_Username';";
     $result = mysqli_query($conn, $sql_statement);
     if (!$result || mysqli_num_rows($result) == 0) {
       //reload the page with different erro
@@ -21,7 +21,14 @@ if($action == 'send'){
       exit();
     }
     $Receiver_UID = mysqli_fetch_row($result)[0];
-    echo $Receiver_UID;
+    $sql_statement = "INSERT INTO Message(Sender_UID,
+    Receiver_UID, Content, Is_Draft, Receiver_Username)
+    Values('$Sender_UID', '$Receiver_UID', '$message', 0, '$Reciever_Username');";
+    $result = mysqli_query($conn, $sql_statement);
+    if(!$result){
+      echo 'Could not run query: ' . mysqli_error($conn);
+    }
+    mysqli_query($conn, $sql_statement);
     exit();
 }
 echo 'save';
