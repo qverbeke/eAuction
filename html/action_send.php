3855369b5f10c12856b577e2c8efa93c1f9b318d
matt <matthewrmancini@gmail.com>
2018-04-11 19:03:37 -0400

    gets the receiver UID from the form

--- html/action_send.php
+++ html/action_send.php
@@ -8,8 +8,20 @@ include_once 'connect-to-database.php';
 $to = $_POST["to"];
 $message = $_POST["message"];
 $action = $_POST["action"];
+
+$Sender_UID = $_SESSION["UID"];
+
 if($action == 'send'){
-    echo 'send';
+    $sql_statement = "SELECT U.UID FROM User_Username U WHERE U.Username='$to'";
+    $result = mysqli_query($conn, $sql_statement);
+    if (!$result || mysqli_num_rows($result) == 0) {
+      //reload the page with different erro
+      header("Location: messages.php?error=to_not_found");
+      //stop running script
+      exit();
+    }
+    $Receiver_UID = mysqli_fetch_row($result)[0];
+    echo $Receiver_UID;
     exit();
 }
 echo 'save';
