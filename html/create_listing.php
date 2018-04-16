dfae6b0c08059d0ec9db262faa5cd9d71ff0da4f
matt <matthewrmancini@gmail.com>
2018-04-16 11:12:47 -0400

    adds create_listing.php with navbar

---
+++ html/create_listing.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>The Better Bookstore</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script>
+  //When document has loaded
+	$(document).ready(function(e){
+    $('#navbar').load("navbar.html");
+	});
+	</script>
+</head>
+<body>
+<div id="navbar" style="margin-top:50px;">
+</div>
+
+
+</body>
