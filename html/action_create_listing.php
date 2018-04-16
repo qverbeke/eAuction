f273b16cc212b3b59903e44365d1cf4115d13bfb
matt <matthewrmancini@gmail.com>
2018-04-16 13:21:46 -0400

    adds untested functionality to insert into the listing table from action_create_listing.php

--- html/action_create_listing.php
+++ html/action_create_listing.php
@@ -6,18 +6,33 @@ error_reporting(E_ALL);
 include_once 'connect-to-database.php';
 
 $action = $_POST["action"];
+if ($action != "book" && $action != "doc"){
+  echo "illegal action";
+  exit();
+}
+$seller_uid = $_SESSION["UID"];
+$price = 0;
+if($action == "book"){
+  $price = $_POST["book-price"];
+}
+else{
+  $price = $_POST["doc-price"];
+}
+$sql_statement = "INSERT INTO Listing(Price, Seller_UID)
+VALUES(\"{$price}\", \"{$seller_uid}\");";
+$result = mysqli_query($conn, $sql_statement);
+if(!$result){
+  if(!$result){
+    echo 'Could not run query: ' . mysqli_error($conn);
+  }
+}
+
 if($action == "book"){
   $isbn = $_POST["select-book"];
-  $price = $_POST["book-price"];
   $quality = $_POST["select-quality"];
-  echo "{$isbn} {$price} {$quality}";
   exit();
 }
 else if($action =="doc"){
-  exit();
-}
-else{
-  echo "illegal action";
   exit();
 }
 
