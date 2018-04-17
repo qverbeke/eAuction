02cf2d3d7458c1f942ec24bc860756b906cd97d1
Quinn Verbeke <ekebrev34@gmail.com>
2018-04-17 18:13:20 -0400

    Browse funcitonality, left off a step away from getting AJAX to work

--- html/browse.php
+++ html/browse.php
@@ -20,6 +20,40 @@
 		});
 	});
   </script>
+  <script>
+	function expandChildren(parent_id){
+		var par_elem = Array.from(document.getElementById(parent_id).children);
+		var i;
+		for(i=1; i<par_elem.length; i++){
+			if(par_elem[i].className==="hide"){
+				par_elem[i].className="unhide";
+			}
+			else if(par_elem[i].className==="unhide"){
+				par_elem[i].className="hide";
+			}
+		}
+		return false;
+	}
+	function findChildCourses(parent_id){
+		var res_ary = findChildCourses_helper(parent_id);
+		$.post('/browse_action.php', {'res_ary', res_ary});
+		return false;
+	}
+	function findChildCourses_helper(parent_id){
+		var par_elem = Array.from(document.getElementById(parent_id).children);
+		var res_ary = [];
+		var i;
+		for(i=1; i<par_elem.length; i++){
+			if(par_elem[i].id.includes("course")){
+				res_ary.push(par_elem[i].id.substring(6));
+			}
+			else if(par_elem[i].id.includes("browse")){
+				res_ary=res_ary.concat(findChildCourses_helper(par_elem[i].id));
+			}
+		}
+		return res_ary;
+	}
+  </script>
 </head>
 <body>
 <div id="navbar" style="margin-top:50px;">
@@ -27,7 +61,7 @@
 <div class="container-fluid with-navbar">
 	<div class="row">
 		<div class="col-sm-6">
-		
+			
 		</div>
 		<div class="col-sm-6">
 			<?php
@@ -42,7 +76,6 @@
 						return array(1, count($GLOBALS["group_hier"]));
 					}
 					else{
-						
 						$in_ary=check_in_array($supergroup_GID["Supergroup_GID"]);
 						if($in_ary[0]!=0){
 							array_splice($GLOBALS["group_hier"], $in_ary[0], 0, array(array($gid, $in_ary[1])));
@@ -65,22 +98,54 @@
 					}
 					return array(0, 0);
 				}
-				function create_my_string($input_tuple){
+				function direct_child_courses($gid){
+					include 'connect-to-database.php';
+					$query="SELECT C.CID, C.Name FROM MyGroup G, Course C WHERE G.GID=".$gid." AND C.GID=G.GID;";
+					$result=mysqli_query($conn, $query);
+					while($c_info = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+						echo "<div id='course".$c_info["CID"]."' class='hide' style='margin-left:10px'>
+							<a>".$c_info["Name"]."</a>
+						</div>";
+					}
+				}
+				function create_graphic($input_index){
+					$input_tuple=array();
+					if($input_index>=count($GLOBALS["group_hier"])){
+						return 0;
+					}
+					else{
+						$input_tuple=$GLOBALS["group_hier"][$input_index];
+					}
 					include 'connect-to-database.php';
 					$query="SELECT Name FROM MyGroup WHERE GID=".$input_tuple[0].";";
 					$result=mysqli_query($conn, $query);
 					$name = mysqli_fetch_assoc($result);
-					return "<h3>".str_repeat("--", $input_tuple[1])." ".$name["Name"]."</h3>";
+					if($GLOBALS["prev_level"]>=$input_tuple[1]){
+						echo str_repeat("</div>", 1+$GLOBALS["prev_level"]-$input_tuple[1]);
+					}
+					$GLOBALS["prev_level"]=$input_tuple[1];
+					echo "<div id='browse".$input_tuple[0]."' style='margin-left:10px' class='";
+					if($input_tuple[1]==0){
+						echo "un";
+					}
+					echo "hide'>
+							<a href='' onclick=\"expandChildren('browse".$input_tuple[0]."'); return false;\">"
+								.$name["Name"]."
+							</a>";
+					direct_child_courses($input_tuple[0]);
+					create_graphic($input_index+1);
+					echo "</div>";
+					return "<h3>".str_repeat("&emsp;", $input_tuple[1])." ".$name["Name"]."</h3>";
 				}
+				
 				include 'connect-to-database.php';
 				$query="SELECT GID FROM MyGroup;";
 				$result=mysqli_query($conn, $query);
 				while($GID = mysqli_fetch_array($result, MYSQLI_ASSOC)){
 					build_hier($GID["GID"]);
 				}
-				foreach($group_hier as $input_tuple){
-					echo create_my_string($input_tuple);
-				}
+				$prev_level=-1;
+				create_graphic(0);
 				
 				
 				
