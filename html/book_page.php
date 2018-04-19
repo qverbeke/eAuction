b7a5c6253f8f97046340bea6b2f71fb0813075b5
Quinn Verbeke <ekebrev34@gmail.com>
2018-04-18 20:10:31 -0400

    Removed sidebar from document information pages

--- html/book_page.php
+++ html/book_page.php
@@ -25,204 +25,148 @@
 <div id="navbar" style="margin-top:50px;">
 </div>
 <div class="container-fluid with-navbar">
-	<div class="row">
-		<div class="col-sm-3">
-			<div class="container-fluid" style="background-color:black; border-radius:20px">
-				<div style="color:white">
-					<br>
-					<?php
-						$hier=array("All Books", "---Penn State University", "------Eberly College of Science",
-									"---------Math 040", "---------Math 140", "---------Math 141",
-									"---------Math 230", "---------Math 250", "---------Math 251",
-									"------College of Engineering", "---------AERO 020", "---------CMPSC 121");
-						foreach($hier as $elem){
-							echo "<a><p style=\"color:white\">$elem</p></a>";
-						}
-					?>
-					<br>
-					<p>Price range:</p>
-					<form>
-						<div class="checkbox active">
-						  <label><input type="checkbox" checked="checked" value="" onchange="var thing = document.getElementsByClassName('pricerange'); for(i=0; i<thing.length; i++){thing[i].disabled = this.checked; thing[i].checked=0;}">Any</label>
-						</div>
-						<div class="checkbox disabled">
-						  <label><input class="pricerange" type="checkbox" value="" disabled>$0-$50</label>
-						</div>
-						<div class="checkbox disabled">
-						  <label><input class="pricerange" type="checkbox" value="" disabled>$50-$100</label>
-						</div>
-						<div class="checkbox disabled">
-						  <label><input class="pricerange" type="checkbox" value="" disabled>$100-$150</label>
-						</div>
-						<div class="checkbox disabled">
-						  <label><input class="pricerange" type="checkbox" value="" disabled>$150-$200</label>
-						</div>
-
-					<p>Search Type</p>
-					<div class="checkbox active">
-					  <label><input type="checkbox" checked="checked" value="">Books</label>
+	<div class="container-fluid" style="background-color:#00cc7a; padding-top:20px;padding-bottom:20px; border-radius:10px">
+		<div class="container-fluid">
+			<form action="/list_page.php">
+				<div class="input-group">
+					<div class="input-group-btn search-panel">
+						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" style="margin-top:-10px">
+							<span id="search_concept">Search by</span> <span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu" role="menu">
+						  <li><a href="#Title">Title</a></li>
+						  <li><a href="#Author">Author</a></li>
+						  <li><a href="#ISBN">ISBN</a></li>
+						  <li class="divider"></li>
+						  <li><a href="#Prof">Professor</a></li>
+						  <li><a href="#Course Name">Course Name</a></li>
+						  <li><a href="#Group">Major/College</a></li>
+						</ul>
 					</div>
-					<div class="checkbox">
-					  <label><input type="checkbox" checked="checked" value="">Past Homeworks</label>
-					</div>
-					<div class="checkbox">
-					  <label><input type="checkbox" checked="checked" value="">Past Exams</label>
-					</div>
-					<div class="checkbox">
-					  <label><input type="checkbox" checked="checked" value="">Miscellaneous Documents</label>
-					</div>
-					<input type="submit" value="Update Preferences">
-					</form>
+					<input type="hidden" name="search_param" value="Title" id="search_param">
+					<input type="text" class="form-control" name="search_term" placeholder="Search term...">
+					<span class="input-group-btn">
+						<input class="btn btn-default" type="submit" value="Search">
+					</span>
 				</div>
-
-			</div>
-		</div>
-		<div class="col-sm-9" style="background-color:#00cc7a; padding-top:20px;padding-bottom:20px; border-radius:10px">
-			<div class="container-fluid">
-				<form action="/list_page.php">
-					<div class="input-group">
-						<div class="input-group-btn search-panel">
-							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" style="margin-top:-10px">
-								<span id="search_concept">Search by</span> <span class="caret"></span>
-							</button>
-							<ul class="dropdown-menu" role="menu">
-							  <li><a href="#Title">Title</a></li>
-							  <li><a href="#Author">Author</a></li>
-							  <li><a href="#ISBN">ISBN</a></li>
-							  <li class="divider"></li>
-							  <li><a href="#Prof">Professor</a></li>
-							  <li><a href="#Course Name">Course Name</a></li>
-							  <li><a href="#Group">Major/College</a></li>
-							</ul>
-						</div>
-						<input type="hidden" name="search_param" value="Title" id="search_param">
-						<input type="text" class="form-control" name="search_term" placeholder="Search term...">
-						<span class="input-group-btn">
-							<input class="btn btn-default" type="submit" value="Search">
-						</span>
-					</div>
 				</form>
-			</div>
-			<hr>
-			<?php
-			include_once 'connect-to-database.php';				
-			$query_piece="";
-			$ISBN=$_GET['ISBN'];
-			$query="SELECT * FROM Book B, Book_NAE N, Book_Name_Desc_Key D WHERE B.ISBN='".$ISBN."' AND B.ISBN=N.ISBN AND B.Name=D.Name AND B.Description=D.Description";
-			$result=mysqli_query($conn, $query);
-			$book_info = mysqli_fetch_array($result, MYSQLI_ASSOC);
-			echo "<div class=\"container-fluid\" style=\"background-color:white; margin:10px 10px 10px 10px; border-radius: 10px\">
-				<div class=\"row\">
-					<div class=\"col-sm-4\" style=\"margin:10px\">
-						<img src=\"".$book_info["ImgURL"]."\" style=\"width:100%\">
+		</div>
+		<hr>
+		<?php
+		include_once 'connect-to-database.php';				
+		$query_piece="";
+		$ISBN=$_GET['ISBN'];
+		$query="SELECT * FROM Book B, Book_NAE N, Book_Name_Desc_Key D WHERE B.ISBN='".$ISBN."' AND B.ISBN=N.ISBN AND B.Name=D.Name AND B.Description=D.Description";
+		$result=mysqli_query($conn, $query);
+		$book_info = mysqli_fetch_array($result, MYSQLI_ASSOC);
+		echo "<div class=\"container-fluid\" style=\"background-color:white; margin:10px 10px 10px 10px; border-radius: 10px\">
+			<div class=\"row\">
+				<div class=\"col-sm-4\" style=\"margin:10px\">
+					<img src=\"".$book_info["ImgURL"]."\" style=\"width:100%\">
+				</div>
+				<div class=\"col-sm-8\" style=\"margin-right:-30px\">
+					<div class=\"row\">
+						<div class=\"col-sm-7\">
+							<h1>".$book_info["Name"]."</h1>
+							<h3>".$book_info["Author"]."</h3>
+							<h4>Edition: ".$book_info["Edition"]."</h4>
+							<h5>".$book_info["Description"]."</h5>
+							<h3>Keywords: ";
+							$keywords = explode(",", $book_info["MyKeys"]);
+							foreach($keywords as $keyword){
+								echo "<a>$keyword</a>, ";
+							}
+					  echo "</h3>
+						</div>
+						<div class=\"col-sm-5\" style=\"padding-right:20px\">
+							<h2>Used in:</h2><hr>";
+							$used_query="SELECT C.Name FROM Book B, Course_Uses_Book U, Course C WHERE B.ISBN='".$book_info["ISBN"]."' AND B.ISBN=U.ISBN AND C.CID=U.CID;";
+							$used_result=mysqli_query($conn, $used_query);
+							while($used_by=mysqli_fetch_assoc($used_result)){
+								echo "<h3>".$used_by["Name"]."</h3>";
+							}
+				 echo "</div>
 					</div>
-					<div class=\"col-sm-8\" style=\"margin-right:-30px\">
-						<div class=\"row\">
-							<div class=\"col-sm-7\">
-								<h1>".$book_info["Name"]."</h1>
-								<h3>".$book_info["Author"]."</h3>
-								<h4>Edition: ".$book_info["Edition"]."</h4>
-								<h5>".$book_info["Description"]."</h5>
-								<h3>Keywords: ";
-								$keywords = explode(",", $book_info["MyKeys"]);
-								foreach($keywords as $keyword){
-									echo "<a>$keyword</a>, ";
-								}
-						  echo "</h3>
+				</div>
+			</div>
+		</div>";
+		$query="SELECT Qty_sold, Avg_price, Min_price_ever, Max_Price_Ever FROM Book WHERE ISBN='".$ISBN."';";
+		$result=mysqli_query($conn, $query);
+		$meta_info=mysqli_fetch_assoc($result);
+		if($meta_info["Qty_sold"]!=0){
+			echo "<div class='container-fluid' style='background-color:white; margin:10px 10px 10px 10px; border-radius: 10px'>
+				<h3><b>Some information about the sale of this book:</b></h3>
+				<div class='row'>
+					<div class='col-sm-3'>
+						<h4>Quantity sold:</h4><h2>".$meta_info["Qty_sold"]."</h2>
+					</div>
+					<div class='col-sm-3'>
+						<h4>Average price:</h4><h2>$".$meta_info["Avg_price"]."</h2>
+					</div>
+					<div class='col-sm-3'>
+						<h4>Minimum price ever:</h4><h2>$".$meta_info["Min_price_ever"]."</h2>
+					</div>
+					<div class='col-sm-3'>
+						<h4>Maximum price ever:</h4><h2>$".$meta_info["Max_Price_Ever"]."</h2>
+					</div>
+				</div>
+			</div>";
+		}			
+		$query="SELECT L.LID, L.Price, BL.Quality, S.Seller_rating FROM Book B, Book_Listing BL, Listing L, Seller S WHERE B.ISBN='".$ISBN."' AND B.ISBN=BL.ISBN AND BL.LID=L.LID AND L.Seller_UID=S.UID;";
+		$result=mysqli_query($conn, $query);
+		while($listing_info = mysqli_fetch_assoc($result)){
+			echo "<div class='container-fluid' style='background-color:white; margin:10px 10px 10px 10px; border-radius: 10px'>
+				<div class='row'>
+					<div class='col-sm-2'>
+						<form method='POST' action='/transaction.php'>
+							<input class='btn btn-primary' style='margin-top:6px; width:100%; font-size:24px; margin-top:13px' type='Submit' value='BUY'>
+							<input type='hidden' name='LID' value='".$listing_info['LID']."'>
+						</form>
+					</div>
+					<div class='col-sm-3' style='margin-top:4px'>
+						<div class='container-fluid'>
+							<div class='row'>
+								<div class='col-sm-6'>
+									<h3 align='right'>Price:</h3>
+								</div>
+								<div class='col-sm-6'>
+									<h3 align='left'><b>$".$listing_info["Price"]."</b></h3>
+								</div>
 							</div>
-							<div class=\"col-sm-5\" style=\"padding-right:20px\">
-								<h2>Used in:</h2><hr>";
-								$used_query="SELECT C.Name FROM Book B, Course_Uses_Book U, Course C WHERE B.ISBN='".$book_info["ISBN"]."' AND B.ISBN=U.ISBN AND C.CID=U.CID;";
-								$used_result=mysqli_query($conn, $used_query);
-								while($used_by=mysqli_fetch_assoc($used_result)){
-									echo "<h3>".$used_by["Name"]."</h3>";
-								}
-					 echo "</div>
+						</div>
+					</div>
+					<div class='col-sm-3' style='margin-top:4px'>
+						<div class='container-fluid'>
+							<div class='row'>
+								<div class='col-sm-6'>
+									<h3 align='right'>Quality:</h3>
+								</div>
+								<div class='col-sm-6'>
+									<h3 align='left'><b>".$listing_info["Quality"]."</b></h3>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class='col-sm-4'>
+						<div class='container-fluid'>
+							<div class='row'>
+								<div class='col-sm-4' style='margin-top:6px'><h4>Seller Rating:</h4></div>
+								<div class='col-sm-8'>";
+									for ($j=0; $j<floor(floatval($listing_info["Seller_rating"])); $j=$j+1){
+										echo "<img src='img/star.png' style='white-space: nowrap; width:20px; margin-bottom:-20px'>";
+									}
+								echo "
+									<h4 style='padding-top:14px'>".$listing_info["Seller_rating"]."/5 stars</h4>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>";
-			$query="SELECT Qty_sold, Avg_price, Min_price_ever, Max_Price_Ever FROM Book WHERE ISBN='".$ISBN."';";
-			$result=mysqli_query($conn, $query);
-			$meta_info=mysqli_fetch_assoc($result);
-			if($meta_info["Qty_sold"]!=0){
-				echo "<div class='container-fluid' style='background-color:white; margin:10px 10px 10px 10px; border-radius: 10px'>
-					<h3><b>Some information about the sale of this book:</b></h3>
-					<div class='row'>
-						<div class='col-sm-3'>
-							<h4>Quantity sold:</h4><h2>".$meta_info["Qty_sold"]."</h2>
-						</div>
-						<div class='col-sm-3'>
-							<h4>Average price:</h4><h2>$".$meta_info["Avg_price"]."</h2>
-						</div>
-						<div class='col-sm-3'>
-							<h4>Minimum price ever:</h4><h2>$".$meta_info["Min_price_ever"]."</h2>
-						</div>
-						<div class='col-sm-3'>
-							<h4>Maximum price ever:</h4><h2>$".$meta_info["Max_Price_Ever"]."</h2>
-						</div>
-					</div>
-				</div>";
-			}			
-			$query="SELECT L.LID, L.Price, BL.Quality, S.Seller_rating FROM Book B, Book_Listing BL, Listing L, Seller S WHERE B.ISBN='".$ISBN."' AND B.ISBN=BL.ISBN AND BL.LID=L.LID AND L.Seller_UID=S.UID;";
-			$result=mysqli_query($conn, $query);
-			while($listing_info = mysqli_fetch_assoc($result)){
-				echo "<div class='container-fluid' style='background-color:white; margin:10px 10px 10px 10px; border-radius: 10px'>
-					<div class='row'>
-						<div class='col-sm-2'>
-							<form method='POST' action='/transaction.php'>
-								<input class='btn btn-primary' style='margin-top:6px; width:100%; font-size:24px; margin-top:13px' type='Submit' value='BUY'>
-								<input type='hidden' name='LID' value='".$listing_info['LID']."'>
-							</form>
-						</div>
-						<div class='col-sm-3' style='margin-top:4px'>
-							<div class='container-fluid'>
-								<div class='row'>
-									<div class='col-sm-6'>
-										<h3 align='right'>Price:</h3>
-									</div>
-									<div class='col-sm-6'>
-										<h3 align='left'><b>$".$listing_info["Price"]."</b></h3>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class='col-sm-3' style='margin-top:4px'>
-							<div class='container-fluid'
-							>
-								<div class='row'>
-									<div class='col-sm-6'>
-										<h3 align='right'>Quality:</h3>
-									</div>
-									<div class='col-sm-6'>
-										<h3 align='left'><b>".$listing_info["Quality"]."</b></h3>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class='col-sm-4'>
-							<div class='container-fluid'>
-								<div class='row'>
-									<div class='col-sm-4' style='margin-top:6px'><h4>Seller Rating:</h4></div>
-									<div class='col-sm-8'>";
-										for ($j=0; $j<floor(floatval($listing_info["Seller_rating"])); $j=$j+1){
-											echo "<img src='img/star.png' style='white-space: nowrap; width:20px; margin-bottom:-20px'>";
-										}
-									echo "
-										<h4 style='padding-top:14px'>".$listing_info["Seller_rating"]."/5 stars</h4>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>";
-			
-			}
-		?>
-		</div>
+		
+		}
+	?>
 	</div>
-</div>
-
 </body>
 
 </html>
--- html/course_doc_page.php
+++ html/course_doc_page.php
@@ -25,150 +25,107 @@
 <div id="navbar" style="margin-top:50px;">
 </div>
 <div class="container-fluid with-navbar">
-	<div class="row">
-		<div class="col-sm-3">
-			<div class="container-fluid" style="background-color:black; border-radius:20px">
-				<div style="color:white">
-					<p>Price range:</p>
-					<form>
-						<div class="checkbox active">
-						  <label><input type="checkbox" checked="checked" value="" onchange="var thing = document.getElementsByClassName('pricerange'); for(i=0; i<thing.length; i++){thing[i].disabled = this.checked; thing[i].checked=0;}">Any</label>
-						</div>
-						<div class="checkbox disabled">
-						  <label><input class="pricerange" type="checkbox" value="" disabled>$0-$50</label>
-						</div>
-						<div class="checkbox disabled">
-						  <label><input class="pricerange" type="checkbox" value="" disabled>$50-$100</label>
-						</div>
-						<div class="checkbox disabled">
-						  <label><input class="pricerange" type="checkbox" value="" disabled>$100-$150</label>
-						</div>
-						<div class="checkbox disabled">
-						  <label><input class="pricerange" type="checkbox" value="" disabled>$150-$200</label>
-						</div>
-
-					<p>Search Type</p>
-					<div class="checkbox active">
-					  <label><input type="checkbox" checked="checked" value="">Books</label>
+	<div class="container-fluid" style="background-color:#00cc7a; padding-top:20px;padding-bottom:20px; border-radius:10px">
+		<div class="container-fluid">
+			<form action="/list_page.php">
+				<div class="input-group">
+					<div class="input-group-btn search-panel">
+						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" style="margin-top:-10px">
+							<span id="search_concept">Search by</span> <span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu" role="menu">
+						  <li><a href="#Title">Title</a></li>
+						  <li><a href="#Author">Author</a></li>
+						  <li><a href="#ISBN">ISBN</a></li>
+						  <li class="divider"></li>
+						  <li><a href="#Prof">Professor</a></li>
+						  <li><a href="#Course Name">Course Name</a></li>
+						  <li><a href="#Group">Major/College</a></li>
+						</ul>
 					</div>
-					<div class="checkbox">
-					  <label><input type="checkbox" checked="checked" value="">Past Homeworks</label>
-					</div>
-					<div class="checkbox">
-					  <label><input type="checkbox" checked="checked" value="">Past Exams</label>
-					</div>
-					<div class="checkbox">
-					  <label><input type="checkbox" checked="checked" value="">Miscellaneous Documents</label>
-					</div>
-					<input type="submit" value="Update Preferences">
-					</form>
+					<input type="hidden" name="search_param" value="Title" id="search_param">
+					<input type="text" class="form-control" name="search_term" placeholder="Search term...">
+					<span class="input-group-btn">
+						<input class="btn btn-default" type="submit" value="Search">
+					</span>
 				</div>
-
-			</div>
+			</form>
 		</div>
-		<div class="col-sm-9" style="background-color:#00cc7a; padding-top:20px;padding-bottom:20px; border-radius:10px">
-			<div class="container-fluid">
-				<form action="/list_page.php">
-					<div class="input-group">
-						<div class="input-group-btn search-panel">
-							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" style="margin-top:-10px">
-								<span id="search_concept">Search by</span> <span class="caret"></span>
-							</button>
-							<ul class="dropdown-menu" role="menu">
-							  <li><a href="#Title">Title</a></li>
-							  <li><a href="#Author">Author</a></li>
-							  <li><a href="#ISBN">ISBN</a></li>
-							  <li class="divider"></li>
-							  <li><a href="#Prof">Professor</a></li>
-							  <li><a href="#Course Name">Course Name</a></li>
-							  <li><a href="#Group">Major/College</a></li>
-							</ul>
-						</div>
-						<input type="hidden" name="search_param" value="Title" id="search_param">
-						<input type="text" class="form-control" name="search_term" placeholder="Search term...">
-						<span class="input-group-btn">
-							<input class="btn btn-default" type="submit" value="Search">
-						</span>
+		<hr>
+		<?php
+		include_once 'connect-to-database.php';				
+		$query_piece="";
+		$LID=$_GET['LID'];
+		$query="SELECT L.Price, CD.Type, CD.Description, CD.Title, CD.Qty_sold, C.Name, C.Professor, C.CID FROM Listing L, Course_Doc_Listing CD, Course_Doc_Part_Of_Course P, Course C WHERE L.LID=".$LID." AND L.LID=CD.LID AND CD.LID=P.LID AND P.CID=C.CID";
+		$result=mysqli_query($conn, $query);
+		$doc_info = mysqli_fetch_array($result, MYSQLI_ASSOC);
+		echo "<div class=\"container-fluid\" style=\"background-color:white; margin:10px 10px 10px 10px; border-radius: 10px\">
+			<h1>".$doc_info["Title"]."</h1>
+			<h3>".$doc_info["Type"]."</h3>
+			<h3>".$doc_info["Price"]."</h3>
+			<h4>Quantity Sold: ".$doc_info["Qty_sold"]."</h4>
+			<h5>".$doc_info["Description"]."</h3>
+			<h3>".$doc_info["Name"]."</h3>
+			<h3>".$doc_info["Professor"]."</h3>
+		</div>";
+		
+		$query="SELECT CD.LID, CD.Title, L.Price FROM Listing L, Course_Doc_Listing CD, Course_Doc_Part_Of_Course P WHERE L.LID=CD.LID and CD.LID=P.LID and P.CID=".$doc_info["CID"].";";
+		$result=mysqli_query($conn, $query);
+		while($listing_info = mysqli_fetch_assoc($result)){
+			echo "<div class='container-fluid' style='background-color:white; margin:10px 10px 10px 10px; border-radius: 10px'>
+				<div class='row'>
+					<div class='col-sm-2'>
+						<form method='GET' action='/course_doc_page.php'>
+						<input class='btn btn-primary' style='margin-top:6px; width:100%; font-size:24px; margin-top:13px' type='Submit' value='BUY'>
+							<input type='hidden' name='LID' value='".$listing_info['LID']."'>
+						</form>
 					</div>
-				</form>
-			</div>
-			<hr>
-			<?php
-			include_once 'connect-to-database.php';				
-			$query_piece="";
-			$LID=$_GET['LID'];
-			$query="SELECT L.Price, CD.Type, CD.Description, CD.Title, CD.Qty_sold, C.Name, C.Professor, C.CID FROM Listing L, Course_Doc_Listing CD, Course_Doc_Part_Of_Course P, Course C WHERE L.LID=".$LID." AND L.LID=CD.LID AND CD.LID=P.LID AND P.CID=C.CID";
-			$result=mysqli_query($conn, $query);
-			$doc_info = mysqli_fetch_array($result, MYSQLI_ASSOC);
-			echo "<div class=\"container-fluid\" style=\"background-color:white; margin:10px 10px 10px 10px; border-radius: 10px\">
-				<h1>".$doc_info["Title"]."</h1>
-				<h3>".$doc_info["Type"]."</h3>
-				<h3>".$doc_info["Price"]."</h3>
-				<h4>Quantity Sold: ".$doc_info["Qty_sold"]."</h4>
-				<h5>".$doc_info["Description"]."</h3>
-				<h3>".$doc_info["Name"]."</h3>
-				<h3>".$doc_info["Professor"]."</h3>
+					<div class='col-sm-3' style='margin-top:4px'>
+						<div class='container-fluid'>
+							<div class='row'>
+								<div class='col-sm-6'>
+									<h3 align='right'>Price:</h3>
+								</div>
+								<div class='col-sm-6'>
+									<h3 align='left'><b>$".$listing_info["Price"]."</b></h3>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class='col-sm-3' style='margin-top:4px'>
+						<div class='container-fluid'
+						>
+							<div class='row'>
+								<div class='col-sm-6'>
+									<h3 align='right'>Quality:</h3>
+								</div>
+								<div class='col-sm-6'>
+									<h3 align='left'><b>".$listing_info["Quality"]."</b></h3>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class='col-sm-4'>
+						<div class='container-fluid'>
+							<div class='row'>
+								<div class='col-sm-4' style='margin-top:6px'><h4>Seller Rating:</h4></div>
+								<div class='col-sm-8'>";
+									for ($j=0; $j<floor(floatval($listing_info["Seller_rating"])); $j=$j+1){
+										echo "<img src='img/star.png' style='white-space: nowrap; width:20px; margin-bottom:-20px'>";
+									}
+								echo "
+									<h4 style='padding-top:14px'>".$listing_info["Seller_rating"]."/5 stars</h4>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>";
-			
-			$query="SELECT CD.LID, CD.Title, L.Price FROM Listing L, Course_Doc_Listing CD, Course_Doc_Part_Of_Course P WHERE L.LID=CD.LID and CD.LID=P.LID and P.CID=".$doc_info["CID"].";";
-			$result=mysqli_query($conn, $query);
-			while($listing_info = mysqli_fetch_assoc($result)){
-				echo "<div class='container-fluid' style='background-color:white; margin:10px 10px 10px 10px; border-radius: 10px'>
-					<div class='row'>
-						<div class='col-sm-2'>
-							<form method='GET' action='/course_doc_page.php'>
-								<input class='btn btn-primary' style='margin-top:6px; width:100%; font-size:24px; margin-top:13px' type='Submit' value='BUY'>
-								<input type='hidden' name='LID' value='".$listing_info['LID']."'>
-							</form>
-						</div>
-						<div class='col-sm-3' style='margin-top:4px'>
-							<div class='container-fluid'>
-								<div class='row'>
-									<div class='col-sm-6'>
-										<h3 align='right'>Price:</h3>
-									</div>
-									<div class='col-sm-6'>
-										<h3 align='left'><b>$".$listing_info["Price"]."</b></h3>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class='col-sm-3' style='margin-top:4px'>
-							<div class='container-fluid'
-							>
-								<div class='row'>
-									<div class='col-sm-6'>
-										<h3 align='right'>Quality:</h3>
-									</div>
-									<div class='col-sm-6'>
-										<h3 align='left'><b>".$listing_info["Quality"]."</b></h3>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class='col-sm-4'>
-							<div class='container-fluid'>
-								<div class='row'>
-									<div class='col-sm-4' style='margin-top:6px'><h4>Seller Rating:</h4></div>
-									<div class='col-sm-8'>";
-										for ($j=0; $j<floor(floatval($listing_info["Seller_rating"])); $j=$j+1){
-											echo "<img src='img/star.png' style='white-space: nowrap; width:20px; margin-bottom:-20px'>";
-										}
-									echo "
-										<h4 style='padding-top:14px'>".$listing_info["Seller_rating"]."/5 stars</h4>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>";
-			
-			}
-			
-		?>
-		</div>
-	</div>
-</div>
+		
+		}
+		
+	?>
+	
 
 </body>
 
