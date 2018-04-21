acf857afd8fbff2f96fbaf9539ccb5c954a0beac
matt <matthewrmancini@gmail.com>
2018-04-21 16:05:20 -0400

    adds new file bought.php with navbar

---
+++ html/bought.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>The Better Bookstore</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles/misc.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script>
+	$(document).ready(function(e){
+    $('#navbar').load("navbar.html");
+	});
+  </script>
+</head>
+<body>
+  <div id='navbar'>
+  </div>
+</body>
