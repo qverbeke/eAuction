16097ed404185dd784c3a67e813ed6d4affeb457
matt <matthewrmancini@gmail.com>
2018-04-21 16:20:17 -0400

    pulls data from transaction table

--- html/bought.php
+++ html/bought.php
@@ -17,4 +17,20 @@
 <body>
   <div id='navbar'>
   </div>
+  <div class="container" style="margin-top:53px;">
+  <?php
+    include_once 'connect-to-database.php';
+    $uid = $_SESSION["UID"];
+    $sql_statement = "Select T.Online_or_live, T.Timestamp, T.LID FROM Transaction T WHERE T.Buyer_UID={$uid}";
+    $result = mysqli_query($conn, $sql_statement);
+    if(!$result){
+      echo 'Could not run query: ' . mysqli_error($conn);
+      exit();
+    }
+    for ($i = 0; $i < mysqli_num_rows($result); $i++){
+      $row = mysqli_fetch_row($result);
+      echo($row[2]);
+    }
+   ?>
+ </div>
 </body>
