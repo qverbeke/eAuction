5a681b7d723f61916eeb049770aa8e0b06c95a43
Quinn Verbeke <ekebrev34@gmail.com>
2018-04-24 18:05:35 -0400

    removed searching script from all pages that did not have the search bar on them

--- html/add_book.php
+++ html/add_book.php
@@ -11,13 +11,6 @@
 	  <script>
 		$(document).ready(function(e){
 		$('#navbar').load("navbar.html");
-			$('.search-panel .dropdown-menu').find('a').click(function(e) {
-				e.preventDefault();
-				var param = $(this).attr("href").replace("#","");
-				var concept = $(this).text();
-				$('.search-panel span#search_concept').text(concept);
-				$('.input-group #search_param').val(param);
-			});
 		});
 	  </script>
 	</head>
--- html/wishlist.php
+++ html/wishlist.php
@@ -18,13 +18,6 @@ if(!isset($_SESSION['UID'])){
   <script>
 	$(document).ready(function(e){
     $('#navbar').load("navbar.html");
-		$('.search-panel .dropdown-menu').find('a').click(function(e) {
-			e.preventDefault();
-			var param = $(this).attr("href").replace("#","");
-			var concept = $(this).text();
-			$('.search-panel span#search_concept').text(concept);
-			$('.input-group #search_param').val(param);
-		});
 	});
   </script>
   <script>
