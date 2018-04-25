93737ad5456e9ef35a31bcf90c95a029ad5e29b7
root <root@ip-172-31-44-73.us-east-2.compute.internal>
2018-04-25 22:13:31 +0000

    minor changes to get file uploads working

--- html/action_create_listing.php
+++ html/action_create_listing.php
@@ -81,13 +81,14 @@ else if($action =="doc"){
   }
   mysqli_commit($conn);
   mysqli_close($conn);
-	$target_dir = "uploads/";
+	$target_dir = "/var/www/html/uploads/";
 	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 		
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+	echo "hi";
+        echo $_FILES["fileToUpload"]["error"];
     }
   //header("Location: home.php");
   //exit();
